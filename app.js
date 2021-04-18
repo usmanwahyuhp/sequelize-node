0c84460c2586a872c454c79d4c870eb525f9f9c0
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-04-18 18:39:52 +0700

    database connection and testing the connection

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const { Sequelize } = require('sequelize');
+const db = new Sequelize('codegig', 'postgres', '123456789', {
+    host: 'localhost',
+    dialect: 'postgres',
+});
+
+db.authenticate()
+    .then(() => console.log('database connected slur!'))
+    .catch(err => console.log('error :' + err))
+
 const app = express();
 
 app.get('/', (req, res) => res.send('index'));
