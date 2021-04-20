d2ebcda8bb2943ab41475876c05dea27dfe65206
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-04-20 16:11:03 +0700

    add router

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('index'));
 
+// gigs routes
+app.use('/gigs', require('./routes/gigs'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
